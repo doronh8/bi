747bb489a86129479d62752aeae12c0700ee4c57
doronh8 <doron.h8j@gmail.com>
2025-06-17 14:35:51 +0300

    fixing bash code 1

--- jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
+++ jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
@@ -10,6 +10,6 @@ cd "$HOME/workspace/bi"
 bash "$HOME/workspace/bi/jobs/kaggle/scheduler/execute_my_kaggle_daily.sh"
 
 #python3 jobs/kaggle/my_kaggle.py  bi-course-461012 --etl-action step --etl-name etl
-python3 "$HOME/workspace/bi/jobs/kaggle/myscript.py" bi-course-461012 --etl-action step --etl-name etl
+python3 jobs/kaggle/my_kaggle.py bi-course-461012 --etl-action step --etl-name etl
 
 
