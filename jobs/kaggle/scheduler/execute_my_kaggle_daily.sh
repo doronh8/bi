c1a38e473f0c810675c2620cc869a919615692e8
doronh8 <doron.h8j@gmail.com>
2025-06-17 16:50:45 +0300

    trying to fix crontab 3

--- jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
+++ jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Run the python script according to our configuration
-cd "$HOME/workspace/bi"
+cd ~/workspace/bi/
 
 
-#bash "$HOME/workspace/bi/jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
+# bash ~/workspace/bi/jobs/kaggle/scheduler/execute_my_kaggle_daily.sh
 
-python3 jobs/kaggle/my_kaggle.py bi-course-461012 --etl-action step --etl-name etl
+python3 jobs/kaggle/my_kaggle.py  bi-course-461012 --etl-action step --etl-name etl
